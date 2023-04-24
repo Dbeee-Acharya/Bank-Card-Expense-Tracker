71ceeef7cac3c53f8c8a5964397fb4131750dcd8
Dbeee-Acharya <dbeee.acharya@gmail.com>
2023-04-24 10:03:10 +0545

    Create main JFrame

--- BankGUI.java
+++ BankGUI.java
@@ -2,13 +2,19 @@
  * The BankGUI class 
  */
 
-import javax.swing.*;
+import javax.swing.*; //Importing the java swing components
 
 public class BankGUI {
     
-    private JFrame frame = new JFrame(); //creating a new JFrame instance
+    private JFrame mainFrame; //creating a new JFrame instance
     
     public BankGUI() {
+        mainFrame = new JFrame("Bank GUI"); // Creating the main JFrame 
         
+        
+        
+        mainFrame.setSize(1200, 672); // setting the width and height of the jframe
+        mainFrame.setLayout(null);  // removing the default layout 
+        mainFrame.setVisible(true); // setting the visibility of mainFrame
     }
 }
